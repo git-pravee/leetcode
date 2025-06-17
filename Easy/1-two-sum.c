18fbad0cb11eb1349ce23adb8f7c223caab52078
git-pravee <praveent2060@gmail.com>
2025-06-18 01:01:20 +0530

    update for dispaly output

--- Easy/1-two-sum.c
+++ Easy/1-two-sum.c
@@ -1,5 +1,6 @@
 // Problem: 1. Two Sum
 // Link: https://leetcode.com/problems/two-sum/description/
+#include<stdio.h>
 #include <stdlib.h>
 /**
  * Note: The returned array must be malloced, assume caller calls free().
@@ -26,4 +27,24 @@ int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
     *returnSize=0;// If no solution found, set return size to 0
     free(result); // Free the allocated memory before returning NULL
     return NULL; // Return NULL if no solution is found
+}
+
+int main()
+{
+    int nums[] = {2, 7, 11, 15};
+    int target = 9;
+    int returnSize = 0;
+
+    int *result = twoSum(nums, 4, target, &returnSize);
+    if(result!=NULL)
+    {
+        printf("Output: [%d, %d]\n", result[0], result[1]);
+        free(result);
+    }
+    else
+    {
+        printf("No solution found.\n");
+    }
+
+    return 0;
 }
